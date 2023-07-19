ed10ccf98099e9e7b01ce3831da28f55ae590202
tyler <tktaormina@gmail.com>
2023-07-18 21:45:38 -0700

    update

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -30,9 +30,9 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
         }
@@ -43,7 +43,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use crate::my_module::transformer;
     use super::Command;
 
     #[test]
